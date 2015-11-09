25c46ec52311da21c02ba96e34bfbe64df42a080
ydocforr <crforrest@smcm.edu>
2015-11-09 14:56:38 -0500

    Newest Version 11/9/2015
    New Update

---
+++ COSC438GameDesignNewREV/TileClasses/InventoryDisplayTileItem.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace COSC438GameDesignNewREV
+{
+    public class InventoryDisplayTileItem : Tile
+    {
+        private int itemKey;
+        public InventoryDisplayTileItem(int key, Rectangle newBox, Game1 game)
+            : base(game)
+        {
+            this.active = true;
+            this.itemKey = key;
+            this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/GeneralTileImages/SlipperyTile.png"));
+            this.Box = newBox;
+        }
+        public int ItemKey
+        {
+            get
+            {
+                return itemKey;
+            }
+            set
+            {
+                itemKey = value;
+            }
+        }
+        public void ActivateGUIDisplay()
+        {
+            switch (this.itemKey)
+            {
+                case 0:
+                    {
+                        this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/ItemImages/MiningAxe.png"));
+                        break;
+                    }
+                case 1:
+                    {
+                        this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/ItemImages/FlashLight.png"));
+                        break;
+                    }
+                case 2:
+                    {
+                        this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/ItemImages/Battery.png"));
+                        break;
+                    }
+                case 3:
+                    {
+                        this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/ItemImages/MedKitTile.png"));
+                        break;
+                    }
+                case 4:
+                    {
+                        this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/ItemImages/MiningAxe.png"));
+                        break;
+                    }
+            }        
+        }
+        public void UNActivateGUIDisplay()
+        {
+            this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/GUIImages/InventoryLocationTile.png"));
+        }
+    }
+}
---
+++ COSC438GameDesignNewREV/TileClasses/OhmDropTile.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace COSC438GameDesignNewREV
+{
+    public class OhmDropTile : Tile
+        {
+            private bool occupied;
+            private double holdVal;
+            public OhmDropTile(int key, Rectangle newBox, Game1 game)
+                : base(game)
+            {
+            this.box = newBox;
+            this.image = Texture2D.FromStream(game.GraphicsDevice, TitleContainer.OpenStream(@"Images/MiniGameTileImages/OhmDropTile.png"));
+            switch (key)
+                {
+                    case 1:
+                        {                         
+                        this.colour = 1;
+                        break;
+                        }
+                case 2:
+                    {                    
+                        this.colour = 2;
+                        break;
+                    }
+                case 3:
+                    {
+                        this.colour = 3;
+                        break;
+                    }
+                }
+            }
+        public bool Occupied
+        {
+            get
+            {
+                return occupied;
+            }
+            set
+            {
+                occupied = value;
+            }
+        }
+        public double HoldVal
+        {
+            get
+            {
+                return holdVal;
+            }
+            set
+            {
+                holdVal = value;
+            }
+        }
+    }
+}
